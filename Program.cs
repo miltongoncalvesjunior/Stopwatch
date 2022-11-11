3a4ea0f49f7dcf16bf94ca06e3e09fd751fb6246
Junior gonçalves <decimo.terceiro13@hotmail.com>
2022-11-11 09:46:00 -0300

    0.3

--- Program.cs
+++ Program.cs
@@ -10,15 +10,56 @@ namespace Stopwatch
             Menu();
         }
 
+
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segundos");
-            Console.WriteLine("M = Minutos");
+            Console.WriteLine("S = Segundos => 10s = 10segundos");
+            Console.WriteLine("M = Minutos => 1m = 1 minuto");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deja contar?");
+
+            //substring pode pegar a quantidade de caracter 
+
+            string data = Console.ReadLine().ToLower();
+
+            // Definindo caracter com char e
+            // Pegando o ultimo caracter que o usuario digitou
+            //Uma Substring em C# é uma sequência contígua de caracteres
+            // dentro de uma string. Em outras palavras, uma substring em
+            // C# é uma parte de uma string.
+
+            char type = char.Parse(data.Substring(data.Length -1,1));
+            int time = int.Parse(data.Substring(0, data.Length - 1 ));
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+
+
+            if (time == 0)
+                System.Environment.Exit(0);
+
+            PreStart (time * multiplier);
+
+
+      
         }
 
+        static void  PreStart(int time)
+        {
+            
+            Console.Clear();
+            Console.WriteLine("Ready....");
+            Thread.Sleep(1000);
+            Console.WriteLine("Set....");
+            Thread.Sleep(1000);
+            Console.WriteLine("Go.....");
+            Thread.Sleep(2500);
+
+            Start(time);
+
+        }
         static void Start( int time)
         {
            
@@ -30,11 +71,14 @@ namespace Stopwatch
                 currentTime++;
                 Console.WriteLine(currentTime);
                 Thread.Sleep(1000);
+                
             }
 
             Console.Clear();
             Console.WriteLine("Stopwatch finalizado");
             Thread.Sleep(2500);
+            Menu();
+
         }
     }
 }
